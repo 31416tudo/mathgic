42c9bd32aa25efcae2a2cbecc3358f62261d93a4
31416tudo <mcginnis1996@hotmail.com>
2017-06-27 13:56:24 -0500

    se agrego la pagina principal y el template del menu

---
+++ resources/views/templates/navbar.php
@@ -0,0 +1,62 @@
+<!Doctype HTML>
+
+<html lang="es" background="">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Mathgic</title>
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<body background="img/facu.jpg">
+	<div class="container">
+  <!-- Topper w/ logo -->
+  <div class="row">
+    
+      <a href="//convertify.io"><img am-TopLogo alt="SECUREVIEW" align="center" src="img/logo.jpg" class="img-responsive"></a>
+    
+  </div> <!-- End Topper -->
+  <!-- Navigation -->
+  <div class="row">
+    <nav class="navbar navbar-inverse" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand visible-xs-inline-block nav-logo" href="/"><img src="/images/logo-dark-inset.png" class="img-responsive" alt=""></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav js-nav-add-active-class">
+            <li><a href="">Inicio</a></li>
+            <li><a href="/">Conoce Más</a></li>
+            <li><a href="">Contacto</a></li>
+            <li><a href="">Nuestro Compromiso</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right js-nav-add-active-class">
+            <li class="visible-xs-block"><a href="/about.html">About Us</a></li>
+            <li class="visible-xs-block"><a href="/careers.html">Careers</a></li>
+            <li class="visible-xs-block"><a href="/press-release.html">Press Release</a></li>
+            <li class="visible-xs-block"><a href="https://susteen7.zendesk.com/hc/en-us">Support</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Inicia Sesión<b class="caret"></b></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="careers.html">Careers</a></li>
+                <li><a href="press-release.html">Press Release</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right hidden-xs">
+            <a type="button" class="navbar-btn btn btn-gradient-blue" am-latosans="bold" href="https://susteen7.zendesk.com/hc/en-us">Regístrate</a>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div>
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>	
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,63 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!Doctype HTML>
 
-        <title>Laravel</title>
+@include ('templates/navbar')
+<link href="css/carousel.css" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img class="first-slide" src="img/libros.jpg" alt="First slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1><div class="dropdown"> 
+                                    <ul class="dropdown-menu" role="menu"> 
+                                        <li>
+                                            <a href="#">Action</a>
+                                        </li>                                         
+                                        <li>
+                                            <a href="#">Another action</a>
+                                        </li>                                         
+                                        <li>
+                                            <a href="#">Something else here</a>
+                                        </li>
+                                        <li class="divider"></li>                                         
+                                        <li>
+                                            <a href="#">Separated link</a>
+                                        </li>
+                                    </ul>                                     
+                                </div>Matematicas para todos</h1>
+                            <p>Mathgic es un entorno de estudio para alumnos de secundaria desarrollado por estudiantes de la Facultad de Ciencias de la Computación BUAP</p>
+                            <p><a class="btn btn-lg btn-primary" href="registro.php" role="button">Regístrate Ahora</a></p>
+                        </div>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="item">
+                    <img class="second-slide" src="img/jardin.jpeg" alt="Second slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1>Dale un vistazo a nuestra oferta</h1>
+                            <p>Mathgic cuenta con más de 10 temas de aritmética, geometría y mucho más, repasa desde la comodidad de tu casa y realiza test de aprendizaje rápido</p>
+                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Conoce Más</a></p>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="item">
+                    <img class="third-slide" src="img/grad.jpg" alt="Third slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1>Prueba Mathgic ahora sin ningún costo</h1>
+                            <p>Mathgic es un proyecto sin fines de lucro, registrate ahora y comienza a reforzar tus conocimientos en matemáticas, ¡serás la envidia de tus compañeros!</p>
+                            <p><a class="btn btn-lg btn-primary" href="compromiso.php" role="button">Conoce nuestro Compromiso</a></p>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>
         </div>
-    </body>
-</html>
